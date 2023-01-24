cf9e89830b8e92386432f915d2d2535c512104e0
Lauren Kessell <lkessell1@gmail.com>
2023-01-24 15:42:04 -0500

    Add Palette component to render main Swatches

---
+++ src/components/MainPalette.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MainSwatch from "./MainSwatch.vue";
+</script>
+
+<template>
+  <section class="main-palette">
+    <MainSwatch />
+  </section>
+</template>
+
+<style scoped>
+.main-palette {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
