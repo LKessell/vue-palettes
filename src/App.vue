575453077290ea088599e7a709ef20fb824acc65
Lauren Kessell <lkessell1@gmail.com>
2023-01-24 14:49:53 -0500

    Set up base layout

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
+import MainSwatch from "./components/MainSwatch.vue";
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1>Vue Paletttes</h1>
   </header>
 
   <main>
-    <TheWelcome />
+    <MainSwatch hex="#4c94b7" />
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+  text-align: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
