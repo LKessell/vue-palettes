c0306e62e0d9fbc20c0925ff6abd3be9bac5b43e
Lauren Kessell <lkessell1@gmail.com>
2023-01-27 17:39:38 -0500

    Add function to generate random hex code

--- src/components/MainPalette.vue
+++ src/components/MainPalette.vue
@@ -28,6 +28,15 @@ function formatPalette(data: { colors: Array<IColor> }) {
   });
 }
 
+function getRandomHex() {
+  const letters = "0123456789ABCDEF";
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 onMounted(() => {
   fetchPalette();
 });
