4af13859ed734f7dce45c7da63c78316d777a3f1
Lauren Kessell <lkessell1@gmail.com>
2023-01-26 19:44:00 -0500

    Use list rendering for Swatches, add temporary data

--- src/components/MainPalette.vue
+++ src/components/MainPalette.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import MainSwatch from "./MainSwatch.vue";
+
+const palette = ref([
+  {
+    isLocked: false,
+    hex: "4c94b7",
+  },
+  {
+    isLocked: false,
+    hex: "BCDE9D",
+  },
+]);
 </script>
 
 <template>
   <section class="main-palette">
-    <MainSwatch />
-    <MainSwatch />
-    <MainSwatch />
-    <MainSwatch />
-    <MainSwatch />
+    <MainSwatch
+      v-for="(color, index) in palette"
+      :color="color"
+      :key="index + color.hex"
+    />
   </section>
 </template>
 
