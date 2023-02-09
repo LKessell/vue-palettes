85811f83943bfb5e46f2bf9f3c63e2a882b4d50b
Lauren Kessell <lkessell1@gmail.com>
2023-02-08 20:12:23 -0500

    Set up base component for saved palette

---
+++ src/components/SavedPalette.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="saved-wrapper">
+    <div class="saved-swatch"></div>
+    <button>Delete</button>
+  </div>
+</template>
+
+<style scoped>
+.saved-wrapper {
+  display: flex;
+}
+.saved-swatch {
+  background-color: #abed79;
+  width: 50px;
+  height: 50px;
+}
+</style>
