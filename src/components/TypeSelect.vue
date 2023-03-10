4f22646b3bef91b909c700ac71b7e7328496a8cf
Lauren Kessell <lkessell1@gmail.com>
2023-03-10 12:32:58 -0500

    Set up TypeSelect component

---
+++ src/components/TypeSelect.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <label for="typeSelect">Palette Type:</label>
+  <select id="typeSelect">
+    <option value="random">Random</option>
+  </select>
+</template>
+
+<style scoped></style>
