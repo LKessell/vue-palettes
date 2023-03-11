88a36ae5078e76915a83bde73d26750169f728c1
Lauren Kessell <lkessell1@gmail.com>
2023-03-10 20:13:03 -0500

    Add array of palette modes, add function to format labels

--- src/components/TypeSelect.vue
+++ src/components/TypeSelect.vue
@@ -1,6 +1,25 @@
 <script setup lang="ts">
 defineProps(["mode"]);
 defineEmits(["update:mode"]);
+
+const modes = [
+  "monochrome",
+  "monochrome-dark",
+  "monochrome-light",
+  "analogic",
+  "complement",
+  "analogic-complement",
+  "triad",
+  "quad",
+];
+
+function getNameLabel(name: string) {
+  const separated = name.split("-");
+  const capitalized = separated.map((word: string) => {
+    return word[0].toUpperCase() + word.substring(1);
+  });
+  return capitalized.join(" ");
+}
 </script>
 
 <template>
