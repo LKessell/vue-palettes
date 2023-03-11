51fef39215ccd3b689232b2b34ab2c0890c62ed0
Lauren Kessell <lkessell1@gmail.com>
2023-03-10 19:47:39 -0500

    Add prop and emit for expanded v-model

--- src/components/TypeSelect.vue
+++ src/components/TypeSelect.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps(["mode"]);
+defineEmits(["update:mode"]);
+</script>
 
 <template>
   <label for="typeSelect">Palette Type:</label>
-  <select id="typeSelect">
+  <select
+    id="typeSelect"
+    :value="mode"
+    @change="$emit('update:mode', $event.target.value)"
+  >
     <option value="random">Random</option>
+    <option value="analogic">Analogic</option>
   </select>
 </template>
 
