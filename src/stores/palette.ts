92776fde10a06ed5d422f00c768849a97eb7ae6d
Lauren Kessell <lkessell1@gmail.com>
2023-03-18 19:06:16 -0400

    Add updatePalette action

--- src/stores/palette.ts
+++ src/stores/palette.ts
@@ -14,4 +14,15 @@ export const usePaletteStore = defineStore("palette", {
   state: (): IPaletteState => ({
     colors: [],
   }),
+  actions: {
+    updatePalette(newColors: IColor[]) {
+      if (!this.colors.length) {
+        this.colors = newColors;
+      } else {
+        this.colors.forEach((color, index) => {
+          if (!color.isLocked) this.colors[index] = newColors[index];
+        });
+      }
+    },
+  },
 });
