47af30443f43332390f6cec8c1a24bbd12f08df1
Lauren Kessell <lkessell1@gmail.com>
2023-05-07 16:04:26 -0400

    Add input label and styles

--- src/components/HexInput.vue
+++ src/components/HexInput.vue
@@ -16,14 +16,31 @@ const borderHex = computed(() => {
 </script>
 
 <template>
-  <input
-    class="hex-entry"
-    :value="modelValue"
-    @blur="$emit('changeFocus', borderHex)"
-    @input="
-      $emit('update:modelValue', ($event.target as HTMLInputElement).value)
-    "
-    placeholder="#000000"
-    :style="{ border: `4px solid ${borderHex}` }"
-  />
+  <div>
+    <label for="hexInput">Seed color:</label>
+    <input
+      id="hexInput"
+      class="hex-entry"
+      :value="modelValue"
+      @blur="$emit('changeFocus', borderHex)"
+      @input="
+        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+      "
+      placeholder="#000000"
+      :style="{ border: `4px solid ${borderHex}` }"
+    />
+  </div>
 </template>
+
+<style scoped>
+.hex-entry {
+  background-color: rgb(233, 233, 237);
+  border-radius: 2rem;
+  font-weight: bold;
+  height: 2.5rem;
+  margin-bottom: 2rem;
+  margin-left: 1rem;
+  text-align: center;
+  transition: border 0.5s;
+}
+</style>
